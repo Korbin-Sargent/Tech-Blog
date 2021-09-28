2a6fdb0b9ada220be42632a9695b7a2780003cc9
Korbin Sargent <korbinksargent@gmail.com>
2021-09-27 23:29:45 -0600

    Building out routes for blogs

--- Controllers/api/blogRoutes.js
+++ Controllers/api/blogRoutes.js
@@ -1,2 +1,126 @@
-const { Blog } = require("../../models");
-//
+const router = require("express").Router();
+const { Blog, User, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+//Route to get all blog posts and associated comments
+router.get("/", (req, res) => {
+  //   console.log("!!!!!!!!!);
+  Post.findAll({
+    attributes: ["id", "title", "postContent", "created_at"],
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: User,
+        attributes: ["UserName"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "commentContent", "blogId", "userId", "created_at"],
+        include: {
+          model: User,
+          attributes: ["UserName"],
+        },
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData.reverse()))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Route to find a single blog post
+
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "postContent", "title", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["UserName"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "commentContent", "blogId", "userId", "created_at"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbBlogData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", withAuth, (req, res) => {
+  Post.create({
+    title: req.body.title,
+    postContent: req.body.content,
+    userId: req.session.userId,
+  })
+    .then((dbBlogData) => res.json(dbBlogData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//route to update a blog post
+
+router.put("/:id", withAuth, (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+      postContent: req.body.content,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbBlogData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Route to delete a blog post
+
+router.put("/:id", withAuth, (req, res) => {
+  Post.delete({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbBlogData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
